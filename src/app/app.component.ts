80285fe6084f92dc4806b82dc7630ec9d8d122f2
Shweta Khandelwal <shweta.khandelwal2012@gmail.com>
2018-05-11 18:08:56 -0500

    first commit NavBar

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform, Icon } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { OrdersPage } from '../pages/orders/orders';
+import { DealsPage } from '../pages/deals/deals';
+import { SettingsPage } from '../pages/settings/settings';
+import { LoginPage } from '../pages/login/login';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  pages: Array<{title: string, component: any,icon: string}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+    // this.icon = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    // 'american-football', 'boat', 'bluetooth', 'build'];
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage,icon:'home'},
+      { title: 'Orders', component: OrdersPage,icon:'pizza'},
+      {title: 'Deals', component: DealsPage,icon:'barcode'},
+      {title: 'Settings', component: SettingsPage,icon:'settings'},
+      {title: 'Login', component: LoginPage,icon:'exit'},
+      
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
